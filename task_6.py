14c88723e07e1e39b7685fff9492cfc536fc92a1
kiri11-mi1 <kirill.mil.0310@mail.ru>
2021-07-21 14:12:07 +0700

    решил на максимум

---
+++ task_6.py
@@ -0,0 +1,12 @@
+# В одномерном массиве найти сумму элементов, находящихся между первым минимальным и последним
+# максимальным элементами. Сами минимальный и максимальный элементы в сумму не включать.
+
+from typing import List
+
+
+def main(numbers_list: List[int]) -> int:
+    ind_min_elem = numbers_list.index(min(numbers_list))
+    ind_max_elem = len(numbers_list) - numbers_list[::-1].index(max(numbers_list)) - 1
+    if ind_min_elem > ind_max_elem:
+        return sum(numbers_list[ind_max_elem+1:ind_min_elem])
+    return sum(numbers_list[ind_min_elem+1:ind_max_elem])
